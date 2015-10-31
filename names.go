0e90d56ab1bee6bf6e41175105ac6f162fc32a13
mpaulweeks <mpaulweeks@gmail.com>
2015-10-31 00:03:05 -0400

    moving things around

---
+++ names.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "net/http"
+    "golang.org/x/net/html"
+)
+
+func getAttr(t html.Token, attr string) (ok bool, val string) {
+    for _, a := range t.Attr {
+        if a.Key == attr {
+            val = a.Val
+            ok = true
+        }
+    }
+    return
+}
+
+func crawl(url string) (ok bool, out string) {
+    resp, err := http.Get(url)
+
+    if err != nil {
+        fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+        return 
+    }
+
+    b := resp.Body
+    defer b.Close() // close Body when the function returns
+
+    z := html.NewTokenizer(b)
+
+    found_it := false
+
+    for {
+        tt := z.Next()
+
+        switch tt {
+        case html.ErrorToken:
+            // End of the document, we're done
+            return
+        case html.StartTagToken:
+            t := z.Token()
+
+            if t.Data != "textarea" {
+                continue
+            }
+
+            ok, id := getAttr(t, "id")
+            if ok && id == "contents" {
+                found_it = true
+            }
+        case html.TextToken:
+            if found_it{
+                ok = true
+                out = string(z.Text())
+                return                
+            }
+        }
+    }
+}
+
+func get_names() []string {
+    names_url := "http://notepad.cc/eti-mm"
+    ok, notepad := crawl(names_url)
+    if !ok {
+        return []
+    }
+    var names []string
+    for _, line := range strings.Split(notepad, "\n"){
+        trimmed := strings.Trim(line, " ")
+        if len(trimmed) == 0 || trimmed[0:1] == "#" {
+            continue
+        }
+        names = append(names, trimmed)
+    }
+    return names
+}
