d5de568f3b6737f8db15e8f16066843785ab0d57
mpaulweeks <mpaulweeks@gmail.com>
2015-11-07 16:31:27 -0500

    add test for nick names

--- users_test.go
+++ users_test.go
@@ -4,7 +4,7 @@ import (
     "testing"
 )
 
-func TestGoogleApp(t *testing.T) {
+func TestGoogleAppMiiverseNames(t *testing.T) {
     cred := loadCredentials(credFileTest)
     ok, res := getUserData(cred)
     if !ok {
@@ -13,5 +13,18 @@ func TestGoogleApp(t *testing.T) {
     expected := "MrLuckyWaffles"
     if !contains(res.MiiverseNames, expected){
         t.Errorf("Expected %q, got %q", expected, res)
+    }
+}
+
+func TestGoogleAppNickNames(t *testing.T) {
+    cred := loadCredentials(credFileTest)
+    ok, res := getUserData(cred)
+    if !ok {
+        t.Errorf("Error on json fetch")
+    }
+    expectedKey := "Waff"
+    expectedValue := "Mr Lucky Waffles"
+    if res.NickNames[expectedKey] != expectedValue {
+        t.Errorf("Expected %q: %q, got %q", expectedKey,expectedValue, res)
     }
 }
